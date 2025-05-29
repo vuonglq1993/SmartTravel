12f2ec8f65b7c7ad69d1bdb4e8fed5dfe1b8a22a
admin <a2@gmail.com>
2025-05-29 11:31:26 +0700

    update admin

--- server/src/main/java/com/smarttravel/server/repository/TourRepository.java
+++
@@ -1,7 +0,0 @@
-package com.smarttravel.server.repository;
-
-import com.smarttravel.server.model.Tour;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface TourRepository extends JpaRepository<Tour, Integer> {
-}
