c6a26d73f4841a38748098dd5b9d3a8f747aac6a
nguyenhuyhoang <nguyenhuyhoang19102005hh@gmail.com>
2025-06-19 08:12:31 +0700

    a

--- admin/src/pages/LoginAdmin.jsx
+++ admin/src/pages/LoginAdmin.jsx
@@ -1,3 +1,5 @@
+// src/pages/LoginAdmin.jsx
+
 import React, { useState } from "react";
 import { useNavigate } from "react-router-dom";
 import { useAuth } from "../context/AuthContext";
@@ -6,31 +8,38 @@ import "../styles/Login.css";
 const Login = () => {
   const { setIsAuthenticated } = useAuth();
   const navigate = useNavigate();
-  const [username, setUsername] = useState("");
+  const [email, setEmail] = useState("");
   const [password, setPassword] = useState("");
 
   const handleLogin = async (e) => {
+    
     e.preventDefault();
 
     try {
-      const response = await fetch("http://localhost:8080/api/login", {
+      const response = await fetch("http://localhost:8080/api/auth/login", {
         method: "POST",
         headers: {
           "Content-Type": "application/json"
         },
-        body: JSON.stringify({ username, password })
+        body: JSON.stringify({ email, password })
       });
 
       const data = await response.json();
+      console.log("Dữ liệu từ API login:", data); // 👈 THÊM DÒNG NÀY
 
-      if (response.ok && data.success) {
-        // Đăng nhập thành công
+      if (response.ok && data.result === true) {
+        if (data.data.role !== "admin") {
+          alert("Tài khoản không có quyền truy cập trang quản trị.");
+          return;
+        }
+
         setIsAuthenticated(true);
         localStorage.setItem("auth", "true");
-        localStorage.setItem("token", data.token || ""); // nếu có trả về token
+        localStorage.setItem("token", data.token);
+        localStorage.setItem("role", data.data.role);
         navigate("/admin/dashboard");
       } else {
-        alert(data.message || "Sai tài khoản hoặc mật khẩu");
+        alert("Sai tài khoản hoặc mật khẩu");
       }
     } catch (error) {
       console.error("Lỗi khi đăng nhập:", error);
@@ -38,16 +47,15 @@ const Login = () => {
     }
   };
 
-  
   return (
     <div className="login-container">
       <form className="login-form" onSubmit={handleLogin}>
         <h2>Admin Login</h2>
         <input
-          type="text"
-          placeholder="Tài khoản"
-          value={username}
-          onChange={(e) => setUsername(e.target.value)}
+          type="email"
+          placeholder="Email"
+          value={email}
+          onChange={(e) => setEmail(e.target.value)}
           required
         />
         <input
